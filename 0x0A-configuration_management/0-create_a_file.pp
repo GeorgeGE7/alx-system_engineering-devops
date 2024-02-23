8bcf9c88db924ecf97a58f8481bf7d5a84d1b777
GeorgeGE7 <elgeo.e.ge.7@gmail.com>
2024-02-23 15:36:02 +0200

    Check the first file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# creates a file in /tmp called school
+# creating a file in /tmp called school
 
 file { '/tmp/school':
-  content =>'I love Puppet',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
